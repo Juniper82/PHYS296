0d7bc03c84a16dff271d866fad132ad4031527ea
zack0179 <31369002+zack0179@users.noreply.github.com>
2018-03-16 11:01:57 -0400

    working on updating bin for Hermes

--- Fibonacci.py
+++ Fibonacci.py
@@ -9,14 +9,14 @@ start_time = time.time()
 num = 2
 a = 1
 b = 1
-print a
-print b
+#print a
+#print b
 while (time.time() - start_time) <= 15.000000000:
     c = a + b
     a = b
     b = c 
     num += 1
-    print c
+#    print c
 print str(num) + ' Fibonacci numbers were computed in %s seconds' % (time.time() - start_time)
 
     
