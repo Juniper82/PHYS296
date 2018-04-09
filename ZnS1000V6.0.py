d9c127e00b81523078e2956fc4b05d135d21b936
zack0179 <31369002+zack0179@users.noreply.github.com>
2018-04-08 21:40:09 -0400

    um idk if this is the newest.

--- ZnS1000V6.0.py
+++ ZnS1000V6.0.py
@@ -21,23 +21,25 @@ from matplotlib.backends.backend_pdf import PdfPages
 # reading the data ##
 #####################
 #dat=pd.read_excel('E:/Laptop/School/Interships/Dr_Prokudin/Juniper82/PHYS296/dat/expdat/1000.xlsx');
-dat=pd.read_excel('C:/Users/Dolam/Documents/Scott/1000.xlsx');
+#dat=pd.read_excel('C:/Users/Aardvark/Documents/GIT/Juniper82/PHYS296/dat/expdat/1000.xlsx');
+dat=pd.read_excel('C:/Users/Tiffany/Documents/Github/Juniper82/PHYS296/dat/expdat/1000.xlsx');
+#dat=pd.read_excel('C:/Users/Dolam/Documents/Scott/1000.xlsx');
 ######################
 # Calculated values ##
 ######################
 dat["delta"] = np.sqrt(dat["stat_u"]**2.0) # measurment error
-dat["qT"] = dat["pT"]/dat["z"] # transverse momentum
-dat["qT2"] = dat["qT"]**2 # square of transverse momentum
+dat["qT"] = dat["pT"]/dat["z"] # transverse momentum **NOTE** not used in plot
+dat["qT2"] = dat["qT"]**2 # square of transverse momentum **NOTE** not used in plot
 
 ##################
 ## Binning data ##
 ##################
 # data bin edeges
 
-xBin=np.array([0.023,0.047,0.075,0.12,0.35,0.6]) # Tick marks figure x-axis
-Q2Bin=np.array([1.0,10]) # Tick marks figure y-axis
-zBin= np.array([0.1, 0.2,0.25,0.3, 0.375, 0.475, 0.6, 0.8, 1.1])
-Phperp = np.array([ 0.0, 0.15, 0.25, 0.35, 0.45, 0.6, 0.8, 1.2]) # catagories for z to be ploted indiviualy in each subplot
+xBin=np.array([0.023,0.047,0.075,0.12,0.35,0.6]) # Bins for x used to plot Pt
+Q2Bin=np.array([1.0,10]) # **NOTE** includes all Q2 data, therefore all value data is plotted vs. Pt
+zBin= np.array([0.1, 0.2,0.25,0.3, 0.375, 0.475, 0.6, 0.8, 1.1]) # catagories for z to be ploted indiviualy in each subplot
+Phperp = np.array([ 0.0, 0.15, 0.25, 0.35, 0.45, 0.6, 0.8, 1.2]) 
 # binning data
 dat['xBin'] = pd.cut(dat['x'], xBin,labels = False, retbins=0)
 dat['Q2Bin'] = pd.cut(dat['Q2'], Q2Bin,labels = False, retbins=0)
@@ -51,36 +53,50 @@ valuedatmod=[0] # Q2bins that overlap xBins
 # HermesPlot ##
 ###############  Reduced matix 
 # initialize the figure
-fig1=py.figure(figsize=(10, 5),facecolor="gray") # figsize; wxh in inche
+fig1=py.figure(figsize=(15, 2.5),facecolor="white") # figsize; wxh in inche
 globalGrid=gridspec.GridSpec(1, 1, wspace=0.0, hspace=0.0) # plot for subplot matrix as 1x1 matrix
 innerGrid=gridspec.GridSpecFromSubplotSpec(1,5 , subplot_spec=globalGrid[0], wspace=0.0, hspace=0.0) # 5x6 subplot grid 
 
 # figure properties:
 ax=fig1.add_axes([0,0,1,1]) # axas for subplot matrix as percent of hole
-ax.yaxis.set_ticks([0,1.25,2.76,4.27,5.78,7.29,8.8,10])
+#ax.yaxis.set_ticks([0,1.25,2.76,4.27,5.78,7.29,8.8,10])
 ax.xaxis.set_ticks([0,1.25,2.8,4.35,5.9,7.45,9,10 ])
 xticklabels = np.array([0,0.023,0.04,0.075,0.1,0.2,0.3,0.6]) 
-yticklabels = np.array([0,1.0, 1.25, 1.75, 2.0, 3.0, 15.0])
-ax.set_yticklabels(yticklabels)
+
+ax.set_yticklabels("")
 ax.set_xticklabels(xticklabels)
 ax.set_xlabel(r"$x$ bins") # xy-lables
-ax.set_ylabel(r"$Q^2$ bins",rotation="horizontal")
-ax.grid() # grid on
+ax.set_ylabel(r"$Q^2$",rotation="horizontal",labelpad = 15)
+#ax.grid() # grid currently off
 
 # subplot axis proerties (unused)
-Zcolor=["red","green","blue","orange","purple","brown"]
+Zcolor=['#F74902',"green","blue",'orange',"purple","brown",'#093162','#4b5320']
 Zmark=["o","o","o","o"]
 
 # Set legend
-Zpatch1 = mpatches.Patch(color=Zcolor[0], label='zbin 0')
-Zpatch2 = mpatches.Patch(color=Zcolor[1], label='zbin 1')
-Zpatch3 = mpatches.Patch(color=Zcolor[2], label='zbin 2')
-Zpatch4 = mpatches.Patch(color=Zcolor[3], label='zbin 3')
-Zpatch5 = mpatches.Patch(color=Zcolor[4], label='zbin 4')
-Zpatch6 = mpatches.Patch(color=Zcolor[5], label='zbin 5')
-ax.legend(handles=[Zpatch1,Zpatch2,Zpatch3,Zpatch4,Zpatch5,Zpatch6],loc='upper left')
+Zpatch1 = mpatches.Patch(color=Zcolor[0], label='0.1<z<0.2')
+Zpatch2 = mpatches.Patch(color=Zcolor[1], label='0.2<z<0.25')
+Zpatch3 = mpatches.Patch(color=Zcolor[2], label='0.25<z<0.3')
+Zpatch4 = mpatches.Patch(color=Zcolor[3], label='0.3<z<0.375')
+Zpatch5 = mpatches.Patch(color=Zcolor[4], label='0.375<z<0.475')
+Zpatch6 = mpatches.Patch(color=Zcolor[5], label='0.475<z<0.6')
+Zpatch7 = mpatches.Patch(color=Zcolor[6], label='0.6<z<0.8')
+Zpatch8 = mpatches.Patch(color=Zcolor[7], label='0.8<z<1.1')
 
-num = 0
+#ax.legend(handles=[Zpatch1,Zpatch2,Zpatch3,Zpatch4,Zpatch5,Zpatch6,Zpatch7,Zpatch8],
+#          bbox_to_anchor=(0, 1),
+#          mode='expand'
+#          )
+          #loc='best')
+#borderaxespad=10
+          
+ax.legend(handles=[Zpatch1,Zpatch2,Zpatch3,Zpatch4,Zpatch5,Zpatch6,Zpatch7,Zpatch8],
+          loc='upper center',
+          ncol=8
+          )
+          #loc='best')
+#borderaxespad=10
+
 for f,F in zip(range(len(pTdatmod)),pTdatmod):       
     for j,J in zip(range(len(valuedatmod)),valuedatmod):
         
@@ -88,30 +104,24 @@ for f,F in zip(range(len(pTdatmod)),pTdatmod):
         maskX = dat['xBin'].isin([F]) # retruns bolinan for all Jth xBin in dat  
         maskY = dat['Q2Bin'].isin([J]) # retruns bolinan for all Fth Q2Bin in dat
        
-        ax = fig1.add_subplot(innerGrid[k]) # add subplot in innerGrid
+        sub_ax = fig1.add_subplot(innerGrid[k]) # add subplot in innerGrid
         xydat = dat[maskX & maskY] # subset of data for Jth xBin and Fth Q2Bin
         for z in range(len(zBin)-1): 
             maskZ = xydat["zBin"].isin([z])
             databin = xydat[maskZ]
             
-            if databin.empty:
-                num += 1
-                ax.set_yticklabels('')
-                ax.set_xticklabels('')
-                pass
+            if f != 0:
+                sub_ax.errorbar(databin['pT'],databin['value'],yerr=databin['delta'],capsize=6,linestyle="",color = Zcolor[z])
+                sub_ax.set_xlabel(r"$p_T$ (GeV)")
+                sub_ax.set_yticklabels('')
+               
             else:
-#                print('xbin = ' + str(f))
-#                print('ybin = ' + str(j))
-#                print('k = ' + str(k))
-#                print(databin)
-                ax.errorbar(databin['pT'],databin['value'],yerr=databin['delta'],capsize=6,linestyle="")
-                if k == 5:
-                    ax.set_xlabel(r"$p_T$ (GeV)")
-                    ax.set_yscale('log')
-                    ax.set_yticklabels("")  
-                else:
-                    ax.set_yscale("log") 
-                    ax.set_xlabel(r"$p_T$ (GeV)")
-pp = PdfPages('mod1000zk.pdf')
-pp.savefig(fig1)
-pp.close()                                 
+                sub_ax.errorbar(databin['pT'],databin['value'],yerr=databin['delta'],capsize=6,linestyle="", color = Zcolor[z])
+                
+                sub_ax.set_xlabel(r"$p_T$ (GeV)")
+                sub_ax.set_yscale('log')
+          
+               
+#pp = PdfPages('mod1000zk.pdf')
+#pp.savefig(fig1)
+#pp.close()                                 
